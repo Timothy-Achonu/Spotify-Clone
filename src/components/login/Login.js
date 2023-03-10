ddcbbbf7df4c3fbb551fc6e2db5bb95869a4c2fb
Timothy-Achonu <achonutimothy@gmail.com>
2023-03-10 12:10:29 +0100

    reducing button font size

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,17 +1,22 @@
-import React, { useContext } from 'react'
-import { AuthContext } from '../../App'
+import React, { useContext } from "react";
+import { AuthContext } from "../../App";
 import { TbOlympics } from "react-icons/tb";
-import styles from './login.module.css';
+import styles from "./login.module.css";
 
 export default function Login() {
-    const setIsLoggedIn = useContext(AuthContext);
+  const setIsLoggedIn = useContext(AuthContext);
   return (
     <div className={styles.container}>
-       <h1>
-        <span> <TbOlympics /> </span>
-           BeeMusic
-        </h1>
-       <button onClick={() => setIsLoggedIn(true)}>connect</button>
+      <h1>
+        <span>
+          {" "}
+          <TbOlympics />{" "}
+        </span>
+        BeeMusic
+      </h1>
+      <button className={styles.connect} onClick={() => setIsLoggedIn(true)}>
+        connect
+      </button>
     </div>
-  )
+  );
 }
